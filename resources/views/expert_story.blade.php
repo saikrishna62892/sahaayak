a8f579f141a0383aaa474b96643c33e63cd88fd4
saikrishna62892 <saikrishna62892@gmail.com>
2020-06-23 09:52:18 +0530

    news & blog section added

---
+++ resources/views/expert_story.blade.php
@@ -0,0 +1,240 @@
+@extends('layout')
+
+    @section('content')
+    <!-- Blog Hero Begin -->
+    <section class="breadcrumb-option blog-hero set-bg" data-setbg="img/breadcrumb.jpg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="blog__hero__text">
+                        <h2>Heading of the Post</h2>
+                        <ul>
+                            <li>November 7, 2019</li>
+                            <li>08 Comments</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Hero End -->
+
+    <!-- Blog Details Section Begin -->
+    <section class="blog-details spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 order-lg-1 order-2">
+                    <div class="blog__sidebar">
+                        <div class="blog__sidebar__search">
+                            <form action="#">
+                                <input type="text" placeholder="Search">
+                                <button><span class="icon_search"></span></button>
+                            </form>
+                        </div>
+                        <div class="blog__sidebar__categories">
+                            <h4>Categories</h4>
+                            <ul>
+                                <li><a href="#">All</a></li>
+                                <li><a href="#">Depression(#count)</a></li>
+                                <li><a href="#">Suicide(#count)</a></li>
+                                <li><a href="#">Stress(#count)</a></li>
+                                <li><a href="#">Stigma(#count)</a></li>
+                                <li><a href="#">Depression(#count)</a></li>
+                                <li><a href="#">Suicide(#count)</a></li>
+                                <li><a href="#">Stress(#count)</a></li>
+                                <li><a href="#">Stigma(#count)</a></li>
+                                <li><a href="#">Stigma(#count)</a></li>
+                            </ul>
+                        </div>
+                        <div class="blog__sidebar__recent">
+                            <h4>Recent News</h4>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-1.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>09 Kinds Of Vegetables Protect The Liver</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-2.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>Tips You To Balance Nutrition Meal Day</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-3.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>4 Principles Help You Lose Weight With Vegetables</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-1.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>09 Kinds Of Vegetables Protect The Liver</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-2.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>Tips You To Balance Nutrition Meal Day</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                            <div class="blog__recent__item">
+                                <div class="blog__recent__item__pic">
+                                    <img src="img/blog/br-3.jpg" alt="">
+                                </div>
+                                <div class="blog__recent__item__text">
+                                    <h6>4 Principles Help You Lose Weight With Vegetables</h6>
+                                    <span>MAR 05, 2019</span>
+                                    <a href="#">More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="blog__sidebar__comment">
+                            <h4>Comment</h4>
+                            <div class="classes__sidebar__comment">
+                                <div class="classes__sidebar__comment__pic">
+                                    <img src="img/classes-details/comment-1.png" alt="">
+                                    <div class="classes__sidebar__comment__rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-half-o"></i>
+                                    </div>
+                                </div>
+                                <div class="classes__sidebar__comment__text">
+                                    <span>04 Mar 2018</span>
+                                    <h6>Brandon Kelley</h6>
+                                    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur,
+                                        adipisci velit,</p>
+                                </div>
+                            </div>
+                            <div class="classes__sidebar__comment">
+                                <div class="classes__sidebar__comment__pic">
+                                    <img src="img/classes-details/comment-2.png" alt="">
+                                    <div class="classes__sidebar__comment__rating">
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star-half-o"></i>
+                                    </div>
+                                </div>
+                                <div class="classes__sidebar__comment__text">
+                                    <span>04 Mar 2018</span>
+                                    <h6>Christina Kelley</h6>
+                                    <p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet,</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-8 order-lg-2 order-1">
+                    <div class="blog__details">
+                        <div class="blog__details__large">
+                            <img src="img/blog/details/blog-large.jpg" alt="">
+                            <span>Tag from user</span>
+                        </div>
+                        <div class="blog__details__text">
+                            <p>Story About the worry </p>
+                        </div>
+                        <div class="blog__details__quote">
+                            <p>This is the content to be highlighted</p>
+                        </div>
+                        <!--<div class="blog__details__pic">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6">
+                                    <img src="img/blog/details/bd-1.jpg" alt="">
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <img src="img/blog/details/bd-2.jpg" alt="">
+                                </div>
+                            </div>
+                        </div>-->
+                        <div class="blog__details__desc">
+                            <h4>Overcoming Solution title</h4>
+                            <p>Solution matter.</p>
+                            <p>Solution matter</p>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6 col-md-6">
+                                <a href="#" class="blog__previous__btn">
+                                    <i class="fa fa-angle-left"></i>
+                                    <h6>6 Reasons to get rebounding</h6>
+                                    <span>Previous Post</span>
+                                </a>
+                            </div>
+                            <div class="col-lg-6 col-md-6">
+                                <a href="#" class="blog__next__btn">
+                                    <i class="fa fa-angle-right"></i>
+                                    <h6>Tips make cooking simple</h6>
+                                    <span>Previous Post</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Blog Details Section End -->
+
+    <!-- Leave Comment Begin -->
+    <div class="leave-comment spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="leave__comment__text">
+                        <h2>Leave A Comment</h2>
+                        <form action="#">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6">
+                                    <input type="text" placeholder="Name*">
+                                </div>
+                                <div class="col-lg-6 col-md-6">
+                                    <input type="text" placeholder="Email*">
+                                </div>
+                                <div class="col-lg-12">
+                                    <div class="leave__comment__rating">
+                                        <h5>Your Rating:</h5>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                        <i class="fa fa-star"></i>
+                                    </div>
+                                </div>
+                                <div class="col-lg-12 text-center">
+                                    <textarea placeholder="Your Comment"></textarea>
+                                    <button type="submit" class="site-btn">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Leave Comment End -->
+
+    @endsection
