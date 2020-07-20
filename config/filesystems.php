6426a0c4904f4bd28da73b7b0151ade768adc069
saikrishna62892 <saikrishna62892@gmail.com>
2020-07-20 23:26:00 +0530

    storage test 1

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,11 +47,11 @@ return [
             'driver' => 'local',
             'root' => storage_path('app'),
         ],
-
+        
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'root' => public_path(),
+            'url' => env('APP_URL').'/public',
             'visibility' => 'public',
         ],
 
