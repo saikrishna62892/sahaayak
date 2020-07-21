a3b16d4dd40f618904b45219d8020a1e54c9d46b
dileep <dileepkumar.nandi9441@gmail.com>
2020-07-21 14:47:58 +0530

    migrations removed

--- storage/framework/views/2799a659b7df6f4fc13b472ace3ac14f3a2b5c94.php
+++
@@ -1,6 +0,0 @@
-
-<?php $__env->startSection('content'); ?>
-	<h4 align="center">Your request is being processed. Please wait for Approval</h4>
-	<a href="\home" style="color:#5768ad;position:relative;">click here to go back</a>
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('layouts.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\Ganesh\Desktop\dileep\sahaayak\resources\views/volunteer/waitingApproval.blade.php ENDPATH**/ ?>
