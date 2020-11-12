a212e262027f1293d2f21c497ca87043ae50f621
saikrishna62892 <saikrishna62892@gmail.com>
2020-11-12 13:57:49 +0530

    Preloader disabled

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -39,9 +39,9 @@
 
 <body>
     <!-- Page Preloder -->
-    <div id="preloder">
+    <!--<div id="preloder">
         <div class="loader"></div>
-    </div>
+    </div>-->
 
     <!-- Offcanvas Menu Begin -->
     <div class="offcanvas-menu-overlay"></div>
