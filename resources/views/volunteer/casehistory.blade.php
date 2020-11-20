01fd81e706a54b077fb5d054ad881754942b3ca5
dileep <dileepkumar.nandi9441@gmail.com>
2020-11-20 21:16:04 +0530

    Get Report donegit add .

--- resources/views/volunteer/casehistory.blade.php
+++ resources/views/volunteer/casehistory.blade.php
@@ -1,15 +1,16 @@
 @foreach($user->appointments as $key => $appointment)
-            <div class="card mt-4 shadow p-4 mb-5 bg-white rounded" >
-                <div class="card-header"></div>
-                    <div class="card-body">
-                        <ul class="list-group">
-                            <li class="list-group-item">
-                                
-                                <div>{{$appointment->casehistory->remarks}}</div>
-                            </li>
-                        </label>
-                
-                        </ul>
-                    </div>
+    <div class="card mt-4 shadow p-4 mb-5 bg-white rounded" >
+        <div class="card-header"></div>
+            <div class="card-body">
+                <ul class="list-group">
+                    <li class="list-group-item">
+                        @if(!is_null($appointment->casehistory ))
+                        <div>{{$appointment->casehistory->remarks}}</div>
+                        @endif
+                    </li>
+                </label>
+        
+                </ul>
             </div>
-            @endforeach
+    </div>
+@endforeach
